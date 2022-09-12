cd02a9eaaf72613ce81c18908deaf09f4d85058d
mahsa-fotros <fotros.mahsa@gmail.com>
2022-09-12 14:37:25 +0430

    added all parts of the project

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const toggler = document.querySelector(".nav__toggler");
+const navbar= document.querySelector(".nav");
+toggler.addEventListener("click", (e)=>{
+    navbar.classList.toggle("nav__expanded");
+});
+
+const tabs= document.querySelectorAll(".tab");
+const tabContents= document.querySelectorAll(".tab-content");
+tabs.forEach((tab)=>{
+    tab.addEventListener("click",(e)=>{
+        const targetTabContent = document.querySelector(
+          `#${tab.dataset.tabTarget}`
+        );
+
+        tabs.forEach((tab)=>tab.classList.remove("active"));
+        tabContents.forEach((tabContent)=>tabContent.classList.remove("active"));
+
+        tab.classList.add("active");
+        targetTabContent.classList.add("active");
+
+    })
+})
+
+
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const accordion = document.querySelector(".accordion");
+accordionHeaders.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.target.parentElement.classList.toggle("accordion__expanded");
+  });
+});
